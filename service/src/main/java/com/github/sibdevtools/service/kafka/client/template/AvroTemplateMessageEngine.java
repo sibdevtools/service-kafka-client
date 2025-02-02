9bab8c93117860a441b0fcad38a8ff92894a232d
sibmaks <sibmaks@vk.com>
2025-02-02 17:20:19 +0300

    [Kafka Client Service] Add AVRO template support #1

---
+++ service/src/main/java/com/github/sibdevtools/service/kafka/client/template/AvroTemplateMessageEngine.java
@@ -0,0 +1,57 @@
+package com.github.sibdevtools.service.kafka.client.template;
+
+import com.github.sibdevtools.error.exception.ServiceException;
+import com.github.sibdevtools.service.kafka.client.constant.Constant;
+import com.github.sibdevtools.service.kafka.client.entity.MessageEngine;
+import org.apache.avro.SchemaParser;
+import org.apache.avro.file.DataFileWriter;
+import org.apache.avro.generic.GenericDatumWriter;
+import org.apache.avro.generic.GenericRecordBuilder;
+import org.springframework.stereotype.Component;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+
+/**
+ * @author sibmaks
+ * @since 0.0.1
+ */
+@Component
+public class AvroTemplateMessageEngine implements TemplateMessageEngine {
+    @Override
+    public byte[] render(byte[] template, Map<String, Object> input) {
+        var schemaParser = new SchemaParser();
+        var schema = schemaParser.parse(new String(template, StandardCharsets.UTF_8));
+        var mainSchema = schema.mainSchema();
+
+        var outputStream = new ByteArrayOutputStream();
+        var datumWriter = new GenericDatumWriter<>(mainSchema);
+        var dataFileWriter = new DataFileWriter<>(datumWriter);
+
+        var recordBuilder = new GenericRecordBuilder(mainSchema);
+        for (var entry : input.entrySet()) {
+            var field = mainSchema.getField(entry.getKey());
+            if (field != null) {
+                recordBuilder.set(field, entry.getValue());
+            }
+        }
+        var message = recordBuilder.build();
+
+        try {
+            dataFileWriter.create(mainSchema, outputStream);
+            dataFileWriter.append(message);
+            dataFileWriter.flush();
+            dataFileWriter.close();
+            return outputStream.toByteArray();
+        } catch (IOException e) {
+            throw new ServiceException(Constant.ERROR_SOURCE, "TEMPLATE_ERROR", "Can't render template", e);
+        }
+    }
+
+    @Override
+    public MessageEngine getEngine() {
+        return MessageEngine.AVRO;
+    }
+}
